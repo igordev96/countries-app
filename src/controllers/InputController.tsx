f310644971d5310cbd7354573329938f33bd5e4a
igordev96 <igordev96@gmail.com>
2024-01-18 00:40:55 +0000

    feat/CAPP-003: Add Input and Select components

---
+++ src/controllers/InputController.tsx
@@ -0,0 +1,18 @@
+import { ComponentProps, forwardRef } from 'react';
+import styles from './InputController.module.scss';
+import { MagnifyingGlass } from '@phosphor-icons/react';
+
+type InputController = ComponentProps<'input'>;
+
+export const InputController = forwardRef<HTMLInputElement, InputController>(
+  (props, ref) => {
+    const { className = '', ...rest } = props;
+
+    return (
+      <div className={styles.container}>
+        <MagnifyingGlass size={16} weight='bold' className={styles.icon} />
+        <input className={`${styles.input} ${className}`} {...rest} ref={ref} />
+      </div>
+    );
+  }
+);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,19 @@
+import { InputController } from '../controllers/InputController';
+import { SelectController } from '../controllers/SelectController';
+import { Regions } from '../types/Regions.enum';
 import styles from './Home.module.scss';
+
+const REGIONS = Object.entries(Regions).map((entry) => ({
+  value: entry[0],
+  name: entry[1],
+}));
 
 export function Home() {
   return (
     <div>
       <div className={styles['search-container']}>
-        <input type='text' />
-        <select name='' id='' />
+        <InputController placeholder='Search for a country...' />
+        <SelectController placeholder='Filter by Region' items={REGIONS} />
       </div>
       <div className={styles.cards}>
         <div className={styles.card} />
